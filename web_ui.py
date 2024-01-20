d30db286d54641d91b692abebc70d820295e1c82
Rick Teuthof <rmt22198@outlook.com>
2024-01-20 14:59:00 +0100

    Fixed k8s stuff

--- web_ui.py
+++ web_ui.py
@@ -1,33 +1,33 @@
 import argparse
+import os
 
 import requests_cache
 from flask import Flask, jsonify, render_template, request
 
 parser = argparse.ArgumentParser(description="Yu-Gi-Oh! Web Application")
 parser.add_argument("--debug", action="store_true", help="Enable debug mode")
-parser.add_argument("--port", type=int, default=3000, help="Port to run the server on")
 args = parser.parse_args()
 
 app = Flask(__name__)
 
-base_url = "http://localhost:5000"
+api_host = os.environ.get("YUGIAPI_SERVICE_PORT_5000_TCP_ADDR", "localhost")
+api_port = os.environ.get("YUGIAPI_SERVICE_PORT_5000_TCP_PORT", 5000)
+api_url = f"http://{api_host}:{api_port}"
+
+app_host = os.environ.get("YUGIWEBAPP_SERVICE_PORT_3000_TCP_HOST", "localhost")
+app_port = os.environ.get("YUGIWEBAPP_SERVICE_PORT_3000_TCP_PORT", 5000)
+app_url = f"http://{app_host}:{app_port}"
+
 card_map = None
 arch_map = None
 set_map = None
+
 session = requests_cache.CachedSession("request_cache")
-
-
-def wait_for_api():
-    print("Waiting for the API to be online...")
-    available = False
-    while not available:
-        response = session.get(f"{base_url}/connection")
-        available = response.status_code == 200
 
 
 def setup_maps():
     global card_map, arch_map, set_map
-    maps = session.get(f"{base_url}/mappings").json()
+    maps = session.get(f"{api_url}/mappings").json()
     card_map = maps["card_map"]
     arch_map = maps["arch_map"]
     set_map = maps["set_map"]
@@ -35,16 +35,13 @@ def setup_maps():
 
 @app.route("/search_card", methods=["GET", "POST"])
 def search_card():
-    print(request.method)
-    print(request.form)
     if request.method == "POST":
         card_id = request.form["card_id"]
         try:
-            res = session.get(f"{base_url}/card_data?id={card_id}")
+            res = session.get(f"{api_url}/card_data?id={card_id}")
             card_data = res.json()
-            print(card_data)
             return render_template("card_result.html", card=card_data[0])
-        except session.exceptions.RequestException as e:
+        except Exception as e:
             return jsonify({"error": str(e)})
     return render_template("search.html")
 
@@ -54,11 +51,10 @@ def search_archetype():
     if request.method == "POST":
         archetype_id = request.form["archetype_id"]
         try:
-            res = session.get(f"{base_url}/arch_data?id={archetype_id}")
+            res = session.get(f"{api_url}/arch_data?id={archetype_id}")
             arch_data = res.json()
-            print(arch_data)
             return render_template("arch_result.html", arch=arch_data[0])
-        except session.exceptions.RequestException as e:
+        except Exception as e:
             return jsonify({"error": str(e)})
     return render_template("search.html")
 
@@ -68,11 +64,10 @@ def search_set():
     if request.method == "POST":
         set_id = request.form["set_id"]
         try:
-            res = session.get(f"{base_url}/set_data?id={set_id}")
+            res = session.get(f"{api_url}/set_data?id={set_id}")
             set_data = res.json()
-            print(set_data)
             return render_template("set_result.html", set=set_data[0])
-        except session.exceptions.RequestException as e:
+        except Exception as e:
             return jsonify({"error": str(e)})
     return render_template("search.html")
 
@@ -96,12 +91,11 @@ def index():
 
 
 if __name__ == "__main__":
-    wait_for_api()
     setup_maps()
 
     if args.debug:
-        app.run(debug=args.debug, port=args.port)
+        app.run(debug=args.debug, port=app_port)
     else:
         from waitress import serve
 
-        serve(app, host="0.0.0.0", port=args.port)
+        serve(app, host="0.0.0.0", port=app_port)
